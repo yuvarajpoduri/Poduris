e1198527d2610281e16759f641f919104641f37b
Yuvaraj Poduri <118831011+yuvarajpoduri@users.noreply.github.com>
2026-01-05 10:07:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,16 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://poduris.onrender.com"
+    ],
+    credentials: true
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
